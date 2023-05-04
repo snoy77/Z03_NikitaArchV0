e8b981b8c331bf7d980fc4f6b1d161acbf0d38b6
snoy77 <snoy77@mail.ru>
2023-05-05 00:35:06 +0500

    bytes

--- main.py
+++ main.py
@@ -6,39 +6,51 @@ print('/////// NekitArch V' + VERSION + "/////////\n")
 Methods = ['Кодирование длин серий (без модификации)']
 
 
-def count_bytes(INPUT_DATA, OUTPUT_DATA):
-    print("Выходное сообщение \"" + OUTPUT_DATA + "\"")
-    print("Размер входного сообщения (количество = виртуальные байты): " + str(len(INPUT_DATA)))
-    print("Размер выходного сообщения (количество = виртуальные байты): " + str(len(OUTPUT_DATA)))
+def count_bytes(INPUT_BYTES, OUTPUT_BYTES):
+    print("Входное сообщение в виде байтов:")
+    print(INPUT_BYTES)
+    print("Размер: " + str(len(INPUT_BYTES)) + " byte")
+    print("\n")
+
+    print("Выходное сообщение в виде байтов:")
+    print(OUTPUT_BYTES)
+    print("Размер: " + str(len(OUTPUT_BYTES)) + " byte")
+    print("\n")
 def Methods_1():
     print("\n========== Кодирование длин серий (без модификации) ==========\n")
     while True:
-
         INPUT_DATA = input("Введите строку для сжатия: ")
-        INPUT_DATA_len = len(INPUT_DATA)
-
-        input_bytes = INPUT_DATA.encode('utf-8')
-
         if INPUT_DATA == '-0-':
             break
 
-        OUTPUT_DATA = ''
-        fc = INPUT_DATA[0] #forst char
-        nc = '' #next char
-        countC = 1 #count char
-        for i in range(1, INPUT_DATA_len):
-            nc = INPUT_DATA[i]
+        #INPUT_DATA_len = len(INPUT_DATA)
+        INPUT_BYTES = INPUT_DATA.encode('utf-8')
+        INPUT_BYTES_LEN = len(INPUT_BYTES)
+
+        OUTPUT_BYTES = bytearray(0)
+
+        fc = INPUT_BYTES[0] #forst byte
+        nc = ''             #next byte
+        countC = 1          #count bytes
+
+        #for i in range(1, INPUT_DATA_len):
+        for i in range(1, INPUT_BYTES_LEN):
+            nc = INPUT_BYTES[i]
 
             if fc == nc:
                 countC = countC + 1
             else:
-                OUTPUT_DATA = OUTPUT_DATA + str(countC) + fc
+                #OUTPUT_DATA = OUTPUT_DATA + str(countC) + fc
+                OUTPUT_BYTES.append(countC)
+                OUTPUT_BYTES.append(fc)
                 fc = nc
                 countC = 1
         #печатаем последний символ
-        OUTPUT_DATA = OUTPUT_DATA + str(countC) + fc
-        print()
-        count_bytes(INPUT_DATA, OUTPUT_DATA)
+        #OUTPUT_DATA = OUTPUT_DATA + str(countC) + fc
+        OUTPUT_BYTES.append(countC)
+        OUTPUT_BYTES.append(fc)
+        count_bytes(INPUT_BYTES, OUTPUT_BYTES)
+        #count_bytes(INPUT_DATA, OUTPUT_DATA)
     print("\n===============================================================\n")
 
 while True:
